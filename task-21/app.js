a9ae0670dc1c2b1af0e29af42fd37884cd5c0c0c
Ramazan976 <esgerli3719@gmail.com>
2025-01-17 00:13:53 +0400

    task-21

---
+++ task-21/app.js
@@ -0,0 +1,8 @@
+console.log("Hello world")
+
+var username = ('Ramazan') 
+console.log(username) //Adın mənimsədildiyi sabit dəyişən//
+
+
+let name = prompt('adınızı və soyadınızı daxil edin 😎')
+console.log(name) //istifadəçinin ad soyadının log a verilməsi//
