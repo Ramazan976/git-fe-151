b0cd58be81a3aee0f0d5739abb871f95f672ace1
Ramazan976 <esgerli3719@gmail.com>
2025-01-27 20:55:59 +0400

    task-24

---
+++ task-24/app.js
@@ -0,0 +1,66 @@
+prompt("iki reqemli eded daxil edin")
+let number = 0
+while ( number <100) {
+    number += 7;
+
+console.log(number);
+
+    if ( number >= 10 && number < 100 ) {
+        console.log(`iki reqemli eded ${number}`)
+    }
+    
+}
+
+
+
+
+let N = parseInt(prompt("N ededini daxil edin"));
+
+ for (let i =  0; N > i; i++) {
+    console.log("l know how to use cycles");
+}
+
+
+
+
+for (let number = 100; number <= 999; number++) {
+    if ( number % 10 === 0)
+        console.log(number)
+
+}
+
+
+let sum = 0;
+for (let number = 10;  number <=99; number++) {
+    if ( number % 2 !== 0){
+        
+    sum += number;
+    }
+ 
+}
+
+console.log(`Bütün iki reqemli tek ededlerin cemi ${sum}`)
+ 
+
+
+
+
+const inputnumber = prompt("bir eded daxil edin")
+
+if (input) {
+    if (isNan(input || input <= 0))
+        console.log("zehmet olmasa bir tam eded daxil edin.");
+
+}
+else {
+    let sum = 0;
+}
+
+for (let number = 100; number <= 999; number++) {
+    if (i % inputnumber)
+        console.log(number)
+
+    
+
+}
+
