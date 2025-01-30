263b2b79042bedefa019a9ce816a62ce0e794005
Ramazan976 <esgerli3719@gmail.com>
2025-01-30 11:54:27 +0400

    task-25

---
+++ task-25/app.js
@@ -0,0 +1,34 @@
+const h1elem = document.getElementsByTagName("h1")[0];
+console.log(h1elem);
+
+
+
+
+let elem = document.querySelector("h1");
+elem.remove();
+
+
+
+let Tag = document.querySelector("ul");
+let newli = document.createElement("li");
+
+newli.textContent = "TITAN RTX";
+Tag.appendChild(newli);
+
+
+
+let text = document.getElementsByClassName("primary");
+for (let i = 0; i < text.length; i++) [
+    text[i].style.color = "red"
+]
+
+
+let primary = document.querySelectorAll (`.primary`);
+
+primary.forEach(element => {
+    console.log(element.textContent)
+    
+});
+
+
+
